f1213a90af57820138c2cda1660f8c64b6b069a5
karanydvv <118754730+karanydvv@users.noreply.github.com>
2023-07-02 18:40:14 +0530

    HomePageCard.jsx

--- src/components/HomePageCard.jsx
+++ src/components/HomePageCard.jsx
@@ -1,17 +1,21 @@
-const HomePageCard = ({ title, img, link }) => {
-    return (
-      <div className="h-[420px] bg-white z-30 m-3">
-        <div className="text-lg xl:text-xl font-semibold ml-4 mt-4">{title}</div>
-        <div className="h-[300px] m-4">
-          <img
-            className="h-[100%] w-[100%] object-cover"
-            src={img}
-            alt="Home card"
-          />
-        </div>
-        <div className="text-xs xl:text-sm text-blue-400 ml-4">{link}</div>
+ const HomePageCard = ({ title, img, link }) => {
+  return (
+    <div className="h-[320px] bg-white z-30 mt-4  object-cover">
+      <div className="text-lg xl:text-xl font-semibold ml-4 mt-4">{title}</div>
+      <div className="h-[200px] m-4">
+        <img
+          className="h-[100%] w-[100%] object-cover "
+          src={img}
+          alt="Home card"
+        />
       </div>
-    );
-  };
-  
-  export default HomePageCard;
+      <div className="text-xs xl:text-sm text-blue-400 ml-4 ">
+        <a href={link}   rel="noopener noreferrer" target="_blank">
+          See more
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default HomePageCard;
